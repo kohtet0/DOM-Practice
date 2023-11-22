7f9df16be5b2c5ed9d5e6847b5ddd113f8b70797
Thwin Lin Htet <thwinlinhtetthwinlinhtet@gmail.com>
2023-11-22 22:47:46 +0630

    i write js but nothing idea in my brain.

--- Project/invoice/src/js/main.js
+++ Project/invoice/src/js/main.js
@@ -3,16 +3,61 @@ const app = document.querySelector("#app");
 const addRecordForm = app.querySelector("#addRecordForm");
 const formSelect = app.querySelector("#formSelect");
 const formInput = app.querySelector("#formInput");
-const addBtn = app.querySelector("#addBtn");
-const tableBody = app.querySelector("#tableBody");
+
+// data
+const products = [
+  {
+    id: 1,
+    name: "Apple",
+    price: 500,
+  },
+  {
+    id: 2,
+    name: "Mango",
+    price: 1500,
+  },
+  {
+    id: 3,
+    name: "Banana",
+    price: 200,
+  },
+  {
+    id: 4,
+    name: "Orange",
+    price: 600,
+  },
+  {
+    id: 5,
+    name: "Lime",
+    price: 100,
+  },
+];
 
 // function
+const productOption = (items) => {
+  items.forEach((item) => {
+    const option = new Option(item.name, item.id);
+    console.log(option);
+    formSelect.append(option);
+  });
+};
+
+// initial render
+productOption(products);
 
 // handler
-const addBtnHandler = (event) => {
+const addRecordFormHandler = (event) => {
   event.preventDefault();
-  console.log("u click");
+  if (formSelect.value != "Choose a fruit") {
+    const currentProduct = products.find(
+      (product) => product.id == formSelect.value
+    );
+    console.log(currentProduct)
+
+
+    addRecordForm.reset()
+  }
 };
 
 // listener
-addBtn.addEventListener("click", addBtnHandler);
+addRecordForm.addEventListener("submit", addRecordFormHandler);

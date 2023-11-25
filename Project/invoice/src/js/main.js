fd4a7c8995dd39fb9872c80a8356f6a8e847dd79
Thwin Lin Htet <thwinlinhtetthwinlinhtet@gmail.com>
2023-11-25 21:25:20 +0630

    responsive ok

--- Project/invoice/src/js/main.js
+++ Project/invoice/src/js/main.js
@@ -86,18 +86,18 @@ const createRow = (id, name, price, quantity) => {
     "new group even:bg-white even:dark:bg-gray-900 odd:bg-gray-50 odd:dark:bg-gray-800 border-b dark:border-gray-700";
 
   tr.innerHTML = `
-      <td class="px-6 py-3 text-center border-r border-neutral-200 td-counter">
+      <td class="px-3 sm:px-6 py-2 sm:py-3 text-center border-r border-neutral-200 td-counter">
       
       </td>
       <td
-      class="px-6 py-3 text-center border-r border-neutral-200 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      class="px-3 sm:px-6 py-2 sm:py-3 text-center border-r border-neutral-200 font-medium text-gray-900 whitespace-nowrap dark:text-white"
       >
       ${name}
       </td>
-      <td class="currentPrice px-6 py-3 border-r border-neutral-200 text-end">
+      <td class="currentPrice px-3 sm:px-6 py-2 sm:py-3 border-r border-neutral-200 text-end">
       ${price}
       </td>
-      <td class="px-6 py-3 border-r border-neutral-200">
+      <td class="px-3 sm:px-6 py-2 sm:py-3 border-r border-neutral-200">
         <div class="flex justify-end items-center gap-3">
           <button type="button" class="active:scale-75 duration-300 decrementBtn bg-neutral-600 rounded-lg opacity-0 group-hover:opacity-100">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-4 h-4 pointer-events-none">
@@ -112,7 +112,7 @@ const createRow = (id, name, price, quantity) => {
           </button>
         </div>
       </td>
-      <td class="px-6 py-3 text-end flex justify-end items-center">
+      <td class="px-3 sm:px-6 py-2 sm:py-3 text-end flex justify-end items-center">
         <button class="delBtn active:scale-75 duration-300 bg-neutral-600 rounded-lg p-1 absolute hidden pointer-events-none group-hover:pointer-events-auto group-hover:translate-x-9 group-hover:block">
           <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-4 h-4 pointer-events-none">
           <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -243,6 +243,7 @@ const printHandler = () => {
   print();
 };
 
+// this code is new product form handler
 const newProductFormHandler = (event) => {
   event.preventDefault();
   const formData = new FormData(newProductForm);
